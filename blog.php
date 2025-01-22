180caabcf48780a210f164d8ce04e8e43be4a6d1
Deshlan Subramani <165919325+Deshlan@users.noreply.github.com>
2025-01-22 16:14:58 +0200

    Add files via upload

---
+++ blog.php
@@ -0,0 +1,151 @@
+
+<html lang="en">
+<?php include "Head.php" ?>
+<body>
+    <section id="header">
+      <div class="personal_logo">Tasko's</div>
+
+      <div>
+        <ul id="navbar">
+          <li><a href="index.php">Home</a></li>
+          <li><a  href="shop.php">Shop</a></li>
+          <li><a class="active" href="blog.php">Blog</a></li>
+          <li><a href="about.php">About</a></li>
+          <li><a href="contact.php">Contact</a></li>
+          <li id="lg-bag">
+            <a href="cart.php"
+              ><i class="fa fa-shopping-basket" aria-hidden="true"></i
+            ></a>
+          </li>
+          <a href="index.php" id="close"><i class="ri-close-circle-line"></i></a>
+          </ul>
+      </div>
+      <div id="mobile">
+        <a href="cart.html"
+          ><i class="fa fa-shopping-basket" aria-hidden="true"></i
+        ></a>
+        <i id="bar" class="fas fa-outdent"></i>
+      </div>
+    </section>
+
+    <section id="page-header" class="blog-header">
+      <h2>More about US</h2>
+      <p>Only the best products kept in our store to satisfy you</p>
+    </section>
+
+    <section id="blog">
+        <div class="blog-box">
+            <div class="blog-img">
+                <img src="IMG/Blog1Easter.jpeg" alt="">
+            </div>
+            <div class="blog-details">
+                <h4>The Easter Season is Arriving!</h4>
+                <p>Easter, a joyous and significant celebration, 
+                  marks the arrival of a vibrant tapestry of traditions and symbolism.
+                  At this time of renewal and joy, 
+                  we're excited to offer our customers delightful deals on our sweetest treats. 
+                  Celebrate Easter with our special discounts and indulge in our delicious confections. 
+                  Let our treats add a touch of sweetness to your celebrations, 
+                  making this season even more special. 
+                  Embrace the spirit of Easter with our exclusive offers and make your festivities truly memorable.</p>
+                <a href="#">Continue Reading</a>
+            </div>
+            <h1>20/04</h1>
+        </div>
+        <div class="blog-box">
+            <div class="blog-img">
+                <img src="IMG/Blog2Winter.jpeg" alt="">
+            </div>
+            <div class="blog-details">
+                <h4>Stay Cozy this Winter! </h4>
+                <p>Winter, a season of serene beauty and cozy moments, 
+                  transforms the world into a frosty wonderland. 
+                  As the chill settles in, embrace the warmth of home, 
+                  snuggled up by the fire with a steaming mug of cocoa.
+                  To add to your cozy times, we're delighted to offer special deals on our sweetest treats.
+                  Indulge in rich chocolates, delightful cookies, and festive confections with our exclusive discounts. 
+                  Let our treats warm your heart and make your winter even sweeter. 
+                  Embrace the magic of winter with our delicious offerings.</p>
+                <a href="#">Continue Reading</a>
+            </div>
+            <h1>15/05</h1>
+        </div>
+        <div class="blog-box">
+            <div class="blog-img">
+                <img src="Img/Blog3Deals.jpeg" alt="">
+            </div>
+            <div class="blog-details">
+                <h4>Deals To suit everyones needs!</h4>
+                <p>To suit everyone's needs, we're delighted to offer special deals on our sweetest treats.
+                  From rich chocolates to delightful cookies and festive confections,
+                  there's something for everyone with our exclusive discounts.
+                  Let our treats bring warmth and joy to your winter, making it even more special. 
+                  Embrace the magic of the season with our delicious offerings.
+                </p>
+                <a href="#">Continue Reading</a>
+            </div>
+            <h1>13/06</h1>
+        </div>
+    </section>
+
+    <section id="pagination" class="section-p1">
+        <a href="#">1</a>
+        <a href="#">2</a>
+        <a href="#">3</a>
+        <a href="#"><i class="fa fa-long-arrow-alt-right"></i></a>
+    </section>
+
+    <section id="newsletter" class="section-p1 section-m1">
+      <div class="newstext">
+        <h4>Sign Up for Newsletters</h4>
+        <p>
+          Get E-mail Updates about new arrivals and
+          <span>Special offers.</span>
+        </p>
+      </div>
+      <div class="form">
+        <input type="text" placeholder="Your email address" />
+        <button class="normal">Sign Up</button>
+      </div>
+    </section>
+
+    <footer class="section-p1">
+      <div class="col">
+        <h4>Contact</h4>
+        <p><strong>Address:</strong>Drive</p>
+        <p><strong>Phone:</strong>Drive</p>
+        <p><strong>Hours:</strong>Drive</p>
+        <div class="follow">
+          <h4>Follow us</h4>
+          <div class="icon">
+            <i class="fab fa-facebook-f"></i>
+            <i class="fab fa-twitter"></i>
+            <i class="fab fa-instagram"></i>
+            <i class="fab fa-youtube"></i>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <h4>About</h4>
+        <a href="#">About us</a>
+        <a href="#">Delivery information </a>
+        <a href="#">Privacy Policy</a>
+        <a href="#">Terms & Conditions</a>
+        <a href="#">Conditions Us</a>
+      </div>
+      <div class="col">
+        <h4>My Account</h4>
+        <a href="#">Sign In</a>
+        <a href="#">View Cart </a>
+        <a href="#">My Wishlist</a>
+        <a href="#">Track My order</a>
+        <a href="#">Help</a>
+      </div>
+      <div class="copyright">
+        <p>Siveshan Subramani</p>
+      </div>
+    </footer>
+
+    <script src="script.js"></script>
+  </body>
+</html>
