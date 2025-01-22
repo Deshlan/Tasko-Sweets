980125313c26dc971657e938e9e54f6c8ee3229a
Deshlan Subramani <165919325+Deshlan@users.noreply.github.com>
2025-01-22 16:16:42 +0200

    Add files via upload

---
+++ Head.php
@@ -0,0 +1,14 @@
+
+<head>
+    <?php
+    include "Connection.php"
+    ?>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tasko's</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@4.3.0/fonts/remixicon.css" rel="stylesheet"/>
+    <script src="https://kit.fontawesome.com/dd8ca4d5e5.js" crossorigin="anonymous"></script>
+</head>
